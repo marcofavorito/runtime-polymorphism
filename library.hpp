1dedbd988b74b834d9cd90a1f35ff1d7c2865a05
MarcoFavorito <marco.favorito@gmail.com>
2020-06-27 16:51:24 +0200

    shared pointer based polymorphism

--- library.hpp
+++ library.hpp
@@ -4,15 +4,16 @@
 #include <memory>
 #include <vector>
 
-using object_t = int;
+class object_t{
+public:
+  virtual ~object_t() {}
+  virtual void draw(std::ostream&, size_t) const = 0;
+};
 
-void draw(const object_t& x, std::ostream& out, size_t position)
-{ out << std::string(position, ' ') << x << std::endl; }
-
-using document_t = std::vector<object_t>;
+using document_t = std::vector<std::shared_ptr<object_t>>;
 
 void draw(const document_t& x, std::ostream& out, size_t position){
   out << std::string(position, ' ') << "<document>" << std::endl;
-  for (const auto& e: x) draw(e, out, position + 2);
+  for (const auto& e: x) e->draw(out, position + 2);
   out << std::string(position, ' ') << "</document>" << std::endl;
 }
