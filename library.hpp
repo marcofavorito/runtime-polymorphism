2c3f92d2a8df3127f675ff917af2e0085783cfd0
MarcoFavorito <marco.favorito@gmail.com>
2020-06-27 17:59:16 +0200

    remove custom copies

--- library.hpp
+++ library.hpp
@@ -13,23 +13,7 @@ void draw(const T& x, std::ostream& out, size_t position){
 class object_t{
 public:
   template <typename T>
-  object_t(T x) : self_(std::make_unique<model<T>>(std::move(x))) { }
-
-  // copy constructor - new object equal to and logically disjoint from, the original
-  object_t(const object_t& x) : self_(x.self_->copy_()) {
-    std::cout << "copy" << std::endl;
-  }
-  // default move constructor
-  // object_t (object_t&& x) noexcept : self_(std::move(x.self_)) { }
-  object_t (object_t&& x) noexcept = default;
-
-  // assignment consistent with copy. Satisfies the 'strong exception guarantee'.
-  // pass sink arguments by value and swap or move into place
-  object_t& operator=(const object_t& x){
-    return *this = object_t(x);
-  }
-  // core issue 1402
-  object_t& operator=(object_t&&) noexcept = default;
+  object_t(T x) : self_(std::make_shared<model<T>>(std::move(x))) { }
 
   friend void draw(const object_t& x, std::ostream& out, size_t position){
     x.self_->draw_(out, position);
@@ -39,20 +23,18 @@ private:
   struct concept_t {
     virtual ~concept_t() = default;
     virtual void draw_(std::ostream&, size_t) const = 0;
-    virtual std::unique_ptr<concept_t> copy_() const = 0;
   };
   template <typename T>
   struct model final : concept_t {
     model(T x) : data_(std::move(x)) { }
-    std::unique_ptr<concept_t> copy_() const override {
-      return std::make_unique<model>(*this);
-    }
     void draw_(std::ostream& out, size_t position) const override {
       draw(data_, out, position);
     }
     T data_;
   };
-  std::unique_ptr<concept_t > self_;
+
+  // shared pointer to immutable object has value semantics
+  std::shared_ptr<const concept_t> self_;
 };
 
 using document_t = std::vector<object_t>;
